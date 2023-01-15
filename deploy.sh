b6e3b0099cba9dea46bab841a65b2e6b995ce915
Jacek <steiner@10g.pl>
2023-01-15 23:21:24 +0100

    acces changed 6

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,11 @@
-git update-index --chmod=+x deploy.sh
-#
-#
-#docker login ghcr.io --username jacekrg --password-stdin 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
-#docker image pull ghcr.io/jacekrg/ideaspro
-#
-#docker container stop ideaspro
-#
-#docker container rm ideaspro
-#
-#docker run -d --name ideaspro --net=host ghcr.io/jacekrg/ideaspro
+
+docker login ghcr.io --username jacekrg --password-stdin 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
+docker image pull ghcr.io/jacekrg/ideaspro
+
+docker container stop ideaspro
+
+docker container rm ideaspro
+
+docker run -d --name ideaspro --net=host ghcr.io/jacekrg/ideaspro
 
 
